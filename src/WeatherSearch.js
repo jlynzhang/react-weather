c8f9400d9a85947f6991cd556a369a84df102c1b
jlynzhang <92702611+jlynzhang@users.noreply.github.com>
2022-03-25 11:21:51 -0500

    Import axios

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,1 +1,11 @@
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
+export default function WeatherSearch() {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+}
